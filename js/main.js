519780d0bb52bbc655b2bfc3499fb869e2933f15
Nick Muravev <murvievnick@gmail.com>
2026-09-18 21:54:51 +0300

    feat: верстка страницы

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const bnrSlider = document.querySelector('.js-bnr-slider');
+    const bnrSlider = document.querySelector('[data-slider="our-life"]');
 
     new Swiper(bnrSlider, {
         slidesPerView: 1,
         autoHeight: true,
+        spaceBetween: 6,
+        navigation: {
+            prevEl: '[data-slider-prev="our-life"]',
+            nextEl: '[data-slider-next="our-life"]'
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            }
+        }
     })
 })
